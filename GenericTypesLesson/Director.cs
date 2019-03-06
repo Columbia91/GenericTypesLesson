f61b08352d4057152087a23c2884e55f2544b77a
Columbia91 <alpha.z91@mail.ru>
2019-03-06 21:40:01 +0600

    Добавьте файлы проекта.

---
+++ GenericTypesLesson/Director.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypesLesson
+{
+    public class Director : Employee, IManager
+    {
+        public string FullName { get; set; }
+        public void DoStaff()
+        {
+            Console.WriteLine($"{FullName} управляет работой");
+        }
+    }
+}
---
+++ GenericTypesLesson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypesLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker
+            {
+                Id = 1,
+                PositionName = "Главный директор по подметанию тротуара",
+                Salary = 8,
+                Nickname = "Вася Одуванчик"
+            };
+            Director director = new Director
+            {
+                Id = 2,
+                PositionName = "Великий и могучий главный директор",
+                Salary = 10000,
+                FullName = "Аркадий Леонтьевич Жмых"
+            };
+
+            Stranger stranger = new Stranger();
+
+            DirectorsCompany<Director> company = new DirectorsCompany<Director>(new List<Director> { director });
+            company.DoWorkTogether();
+        }
+    }
+}
